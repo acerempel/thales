0307ccf594fa2c9eb29378bfdf470f03068a7894
Alan Rempel <alan.rempel@gmail.com>
2020-02-03 17:11:36 -0400

    Add Display1 and DisplayH classes
    I.e., add the necessary machinery to make Display work with the new
    ExprH

--- src/Display.hs
+++ src/Display.hs
@@ -2,6 +2,7 @@ module Display where
 
 import Prelude hiding (group)
 
+import Data.Kind
 import Data.Text.Prettyprint.Doc
 import qualified Data.Vector as Vec
 
@@ -13,31 +14,36 @@ data Precedence = Tight | Loose
 
 class Display a where
   display :: Precedence -> a -> Doc Markup
+  default display :: (a ~ h f, DisplayH h, Display1 f) => Precedence -> a -> Doc Markup
+  display = hoistDisplay
 
-instance Display a => Display (ExprF a) where
-  display prec = \case
-    LiteralE lit -> display prec lit
+instance Display1 f => Display (ExprH f)
+
+class Display1 (f :: Type -> Type) where
+  liftDisplay :: (Precedence -> a -> Doc Markup) -> Precedence -> f a -> Doc Markup
+
+class DisplayH (h :: (Type -> Type) -> Type) where
+  hoistDisplay :: Display1 f => Precedence -> h f -> Doc Markup
+
+instance DisplayH ExprH where
+  hoistDisplay prec = \case
+    LiteralE lit ->
+      display prec lit
     ArrayE vec ->
           lbracket
       <+> (align . group . vsep . punctuate comma)
-          ((Vec.toList . Vec.map (display Loose)) vec)
+          ((Vec.toList . Vec.map (liftDisplay display Loose)) vec)
       <+> rbracket
     ApplyE f a ->
       let parenthesizeMaybe =
             case prec of
               Tight -> parens
               Loose -> id
-      in parenthesizeMaybe $ sep [ display Loose f, display Tight a ]
+      in parenthesizeMaybe $ sep [ liftDisplay display Loose f, liftDisplay display Tight a ]
     FieldAccessE n a ->
-      group $ display Tight a <> line' <> dot <> pretty n
+      group $ liftDisplay display Tight a <> line' <> dot <> pretty n
     NameE n ->
       pretty n
-
-instance Display Expr where
-  display prec (Expr expr) = display prec expr
-
-instance (Display a, Display b) => Display (Either a b) where
-  display prec = either (display prec) (display prec)
 
 instance Display Literal where
   display _prec = \case
