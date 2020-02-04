2aa78f41c3a40421e53e9660e2c0f693c2895336
Alan Rempel <alan.rempel@gmail.com>
2020-02-04 10:55:17 -0400

    Add field access test

--- tests-e/Evaluation.hs
+++ tests-e/Evaluation.hs
@@ -9,8 +9,14 @@ import Value
 
 main = hspec $ do
   describe "Evaluation" $ do
-    it "evluates names to their bindings" $ do
+    it "evaluates names to their bindings" $ do
       let expr = NameE "potato"
           bindings = Map.singleton "potato" (Number 3)
       result <- runEvalM (evalTopExpr expr) bindings
       result `shouldBe` Right (Number 3)
+    it "evaluates field accesses" $ do
+      let expr = FieldAccessE "potato" (Id (NameE "vegetables"))
+          bindings =
+            Map.singleton "vegetables" (Record (Map.singleton "potato" (Number 7)))
+      result <- runEvalM (evalTopExpr expr) bindings
+      result `shouldBe` Right (Number 7)
