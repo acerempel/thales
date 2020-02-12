5b10633a72ad296860e74e3215300c0c0c9bbf7f
Alan Rempel <alan.rempel@gmail.com>
2020-02-11 23:55:39 -0400

    Add a test for evaluation of applications

--- tests-e/Evaluation.hs
+++ tests-e/Evaluation.hs
@@ -17,6 +17,15 @@ main = hspec $ do
     it "evaluates field accesses" $ do
       let expr = FieldAccessE "potato" (Id (NameE "vegetables"))
           bindings =
-            Map.singleton "vegetables" (Record (Map.singleton "potato" (Number 7)))
+            Map.singleton "vegetables"
+            (Record (Map.singleton "potato" (Number 7)))
       result <- runEvalM (evalTopExpr expr) bindings
       result `shouldBe` Right (Number 7)
+    it "evaluates function calls" $ do
+      let expr =
+            ApplyE (Id (NameE "plus-one")) (Id (LiteralE (NumberL 4)))
+          bindings =
+            Map.singleton "plus-one"
+            (Function NumberT (return . Number . (+1)))
+      result <- runEvalM (evalTopExpr expr) bindings
+      result `shouldBe` Right (Number 5)
