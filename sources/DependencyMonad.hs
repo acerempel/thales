a8bded4676c957b39ca552f556b23cc4aa7dd5c8
Alan Rempel <alan.rempel@gmail.com>
2020-03-23 12:24:33 -0300

    Generalize 'lookupYaml' to also read markdown
    'YamlQ' is now 'FieldAccessQ' -- the corresponding rule can get
    values either from a yaml file or from a markdown file. If a
    markdown file, key-value pairs are read from the yaml front matter,
    and the 'body' key contains the document body.

--- sources/DependencyMonad.hs
+++ sources/DependencyMonad.hs
@@ -9,10 +9,13 @@ import Control.Exception
 import Data.Binary
 import qualified Data.HashMap.Strict as Map
 import qualified Data.List.NonEmpty as NonEmpty
+import qualified Data.Text.IO as Text
 import qualified Data.Yaml as Yaml
 import Development.Shake
 import Development.Shake.Rule
 import qualified System.Directory as System
+import Text.Megaparsec
+import Text.MMark as MMark
 
 import Parse
 import Value
@@ -29,7 +32,7 @@ instance DependencyMonad Action where
 
   listDirectory = getDirectoryContents
 
-  lookupYaml path key = apply1 (YamlQ path key)
+  lookupYaml path key = apply1 (FieldAccessQ YamlFile path key)
 
 -- | The command-line options.
 data Options = Options
@@ -48,16 +51,17 @@ data RebuildUnconditionally
 run :: Options -> Rules () -> IO ()
 run options rules =
   shake (optionsToShakeOptions options) $ do
-    readValueFromYamlFile <- newCache $ \path -> do
-      need [path]
-      Yaml.decodeFileThrow path
-    addBuiltinRule noLint noIdentity (yamlRun readValueFromYamlFile)
+    readYamlCached <- newCache readYaml
+    readMarkdownCached <- newCache readMarkdown
+    addBuiltinRule noLint noIdentity (fieldAccessRuleRun readYamlCached readMarkdownCached)
     rules
+
   where
     optionsToShakeOptions Options{..} =
       shakeOptions
         { shakeRebuild = translateRebuild optRebuildUnconditionally
         , shakeTimings = optTimings }
+
     translateRebuild optRebuild =
       case optRebuild of
         Just Everything ->
@@ -67,29 +71,65 @@ run options rules =
         Nothing ->
           []
 
-data YamlQ = YamlQ
-  { yamlPath :: FilePath
+    readYaml path = do
+      need [path]
+      YamlValue <$> Yaml.decodeFileThrow path
+
+    readMarkdown path = do
+      need [path]
+      contents <- liftIO $ Text.readFile path
+      let parsed = MMark.parse path contents
+      mmark <-
+        case parsed of
+          Left err -> liftIO $ throwIO (MMarkException err)
+          Right mmark -> return mmark
+      let yaml = fromMaybe (Yaml.Object Map.empty) (MMark.projectYaml mmark)
+      let mb_val = Yaml.parseEither Yaml.parseJSON yaml
+      record <-
+        case mb_val of
+          Left err -> liftIO $ throwIO (Yaml.YamlException err)
+          Right (Record hashmap) -> return hashmap
+          Right _ -> liftIO $ throwIO $ NotAnObject MarkdownFile path
+      let record' = Map.insert "body" (Markdown mmark) record
+      return $ MarkdownValue $ Record record'
+
+data FieldAccessQ = FieldAccessQ
+  { faFileType :: FileType
+  , faFilePath :: FilePath
     -- ^ The path to a YAML file. This is assumed to be an absolute path.
-  , yamlKey :: Text
+  , faField :: Text
   } deriving stock ( Generic, Show, Eq, Typeable )
     deriving anyclass ( Hashable, Binary, NFData )
 
-type instance RuleResult YamlQ = Maybe Value
+type instance RuleResult FieldAccessQ = Maybe Value
 
-yamlRun ::
-  (FilePath -> Action Value) ->
-  YamlQ ->
+data FileType
+  = YamlFile
+  | MarkdownFile
+  deriving stock ( Eq, Show, Generic )
+  deriving anyclass ( Hashable, NFData, Typeable, Binary )
+
+newtype YamlValue = YamlValue { fromYaml :: Value }
+newtype MarkdownValue = MarkdownValue { fromMarkdown :: Value }
+
+fieldAccessRuleRun ::
+  (FilePath -> Action YamlValue) ->
+  (FilePath -> Action MarkdownValue) ->
+  FieldAccessQ ->
   Maybe ByteString ->
   RunMode ->
   Action (RunResult (Maybe Value))
-yamlRun readValueFromYamlFile YamlQ{..} mb_stored mode = do
-  mb_record <- readValueFromYamlFile yamlPath
+fieldAccessRuleRun getYaml getMarkdown FieldAccessQ{..} mb_stored mode = do
+  mb_record <-
+    case faFileType of
+      YamlFile -> fromYaml <$> getYaml faFilePath
+      MarkdownFile -> fromMarkdown <$> getMarkdown faFilePath
   val <-
     case mb_record of
       Record hashMap ->
-        return $ Map.lookup yamlKey hashMap
+        return $ Map.lookup faField hashMap
       _ ->
-        liftIO $ throwIO $ NotAnObject YamlQ{..}
+        liftIO $ throwIO $ NotAnObject faFileType faFilePath
   case mb_stored of
     Nothing ->
       return $ RunResult
@@ -113,10 +153,17 @@ yamlRun readValueFromYamlFile YamlQ{..} mb_stored mode = do
         (toStrict (encode new_hash))
         val
 
-newtype NotAnObject = NotAnObject YamlQ
-  deriving ( Show, Typeable )
+data NotAnObject = NotAnObject FileType FilePath
+  deriving stock ( Show, Typeable )
 
 instance Exception NotAnObject
+
+newtype MMarkException = MMarkException (ParseErrorBundle Text MMarkErr)
+  deriving stock ( Show, Typeable )
+
+instance Exception MMarkException where
+  displayException (MMarkException err) =
+    errorBundlePretty err
 
 instance DependencyMonad IO where
   listDirectory dir = sort <$> System.listDirectory dir
